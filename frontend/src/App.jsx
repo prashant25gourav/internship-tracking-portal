eccb2e11bfe6fdbde83fd521be9ab376f90527dd
Bhavika Chandar <bhavika.chandar02@gmail.com>
2026-05-31 12:20:48 +0530

    feat: finalize all core faculty and student UI layouts with responsive tables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import BrowseJobs from './pages/Student/BrowseJobs';
-import UploadReport from './pages/Student/UploadReport';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Common/Login';
 import StudentDashboard from './pages/Student/StudentDashboard';
+import BrowseJobs from './pages/Student/BrowseJobs';
+import UploadReport from './pages/Student/UploadReport';
 import AdminDashboard from './pages/Admin/AdminDashboard';
+import VerifyApplications from './pages/Admin/VerifyApplications';
+import ReviewReports from './pages/Admin/ReviewReports';
 
 function App() {
   return (
@@ -12,10 +14,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/student" element={<StudentDashboard />} />
-        {/* This matches the click navigation we added to the sidebar! */}
         <Route path="/browse-jobs" element={<BrowseJobs />} /> 
         <Route path="/upload-report" element={<UploadReport />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        {/* New faculty routing layouts */}
+        <Route path="/verify-applications" element={<VerifyApplications />} />
+        <Route path="/review-reports" element={<ReviewReports />} />
       </Routes>
     </Router>
   );
