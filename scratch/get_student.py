658b266e5651bb34983c013d289925fc89e15454
mohanmj2457 <sharmamohan2457@gmail.com>
2026-06-04 05:38:47 +0530

    Hello done with eveything final deploy

--- scratch/get_student.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'backend'))
-try:
-    from db_config import cursor
-    cursor.execute("SELECT Email FROM STUDENT LIMIT 1;")
-    print("Student Email:", cursor.fetchone())
-except Exception as e:
-    print(e)
--- scratch/get_student_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-try:
-    r = requests.get('http://127.0.0.1:5000/students')
-    data = r.json().get('data', [])
-    if data:
-        print("Student email to test:", data[0]['Email'])
-except Exception as e:
-    print(e)
